ce3289b6f05df0848371cf3403d980bcf569ccb8
angry-dev <98198645+angry-dev@users.noreply.github.com>
2023-05-21 19:56:59 +0900

    Widget_Tour 프로젝트 생성

--- Widget_tour/ios/Flutter/flutter_export_environment.sh
+++ Widget_tour/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/hyesungjeon/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/hyesungjeon/Desktop/Flutter/Flutter_Study/Widget_tour"
+export "FLUTTER_APPLICATION_PATH=/Users/hyesungjeon/Desktop/Flutter/Flutter_Study/Widget_Tour"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
